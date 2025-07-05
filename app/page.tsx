f01c00003e150c1b619bdc94a7040ab411bc7b9a
Nader Mandalawi <nader.mandalawi@gmail.com>
2025-07-05 20:53:02 +0300

    fix build errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
+import { Suspense } from "react";
 import PlaygroundEditor from "@/components/playground";
 
 export default function Home() {
   return (
     <div className="min-h-[calc(100vh-12rem)] ">
       <div className="container mx-auto px-4 py-8">
-        <PlaygroundEditor />
+        <Suspense fallback={<div>Loading...</div>}>
+          <PlaygroundEditor />
+        </Suspense>
       </div>
     </div>
   );
